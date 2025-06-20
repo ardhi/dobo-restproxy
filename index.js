5643b2fe624c009dc2ab23c1c4d95a2c8e21ad93
Ardhi Lukianto <ardhi@lukianto.com>
2025-06-20 22:08:57 +0700

    Updates

---
+++ index.js
@@ -0,0 +1,30 @@
+async function factory (pkgName) {
+  const me = this
+
+  return class DoboRestproxy extends this.lib.BajoPlugin {
+    constructor () {
+      super(pkgName, me.app)
+      this.alias = 'dbrpx'
+      this.dependencies = ['dobo', 'bajo-extra']
+      this.config = {
+      }
+    }
+
+    fetch = async (url, opts = {}, extra = {}) => {
+      const { fetchUrl } = this.app.bajoExtra
+      extra.rawResponse = true
+      const resp = await fetchUrl(url, opts, extra)
+      const result = await resp.json()
+      if (!resp.ok) {
+        throw this.error(result.message, {
+          noTrans: true,
+          statusCode: resp.status,
+          success: false
+        })
+      }
+      return result
+    }
+  }
+}
+
+export default factory

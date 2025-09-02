a6a93dec9061d441e89acedd45b87de50a4710e5
Ardhi Lukianto <ardhi@lukianto.com>
2025-09-02 23:48:32 +0700

    Updates

--- extend/dobo/boot/driver.js
+++ extend/dobo/boot/driver.js
@@ -4,13 +4,13 @@ async function driver () {
   const type = ['bajo', 'custom']
   const driver = 'restproxy'
   await eachPlugins(async function ({ file }) {
-    const { name: ns } = this
+    const { ns } = this
     const cfg = readJson(file)
     if (!cfg.type) return undefined
     if (isString(cfg.type)) cfg.type = [cfg.type]
     cfg.type = cfg.type.map(t => `${t}@${ns}`)
     type.push(...cfg.type)
-  }, { glob: 'boot/driver.json', prefix: this.name })
+  }, { glob: 'boot/driver.json', prefix: this.ns })
   return { type, driver }
 }
 
